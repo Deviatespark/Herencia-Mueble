2aea293e66b19bd6ba64715556aacd09738bdddf
galve <galvez.jangofett@gmail.com>
2022-07-22 19:49:52 +0200

    V2.0 Primer commit

--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
 from models.Mesa import Mesa# from directorio.archivo import Mi clase importamos la clase de tal forma
 
 class Main():
-
+   print("Version 2.0 del ejercicio")
    largo = 170
    ancho = 80
    alto = 120
